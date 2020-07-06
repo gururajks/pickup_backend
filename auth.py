2e8cd394874644458ef0ffaa41c1689413326256
Gururaj Sridhar <gsridhar@duosecurity.com>
2020-07-06 11:20:08 -0500

    adding setup.sh and adding environment variables

--- auth.py
+++ auth.py
@@ -3,11 +3,11 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
-
-AUTH0_DOMAIN = 'fsndudacityguru.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'pickupauth'
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+ALGORITHMS = [os.getenv("ALGORITHMS")]
+API_AUDIENCE = os.getenv("API_AUDIENCE")
 
 # AuthError Exception
 '''
---
+++ setup.sh
@@ -0,0 +1,7 @@
+export AUTH0_DOMAIN="fsndudacityguru.auth0.com"
+export API_AUDIENCE="pickupauth"
+export ALGORITHMS="RS256"
+export DATABASE_URL="postgresql://gsridhar@localhost:5432/pickup"
+
+export FLASK_APP=app.py
+flask run
